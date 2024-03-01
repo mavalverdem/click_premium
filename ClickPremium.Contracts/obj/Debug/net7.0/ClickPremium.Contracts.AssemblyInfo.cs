fcd99b7fbb7781ec07f7b8dc7cfb615c21c81590
Julio Ayasta Grande <creatorcapac@Julios-MacBook-Air.local>
2024-03-01 11:50:37 -0800

    initial 2

--- ClickPremium.Contracts/obj/Debug/net7.0/ClickPremium.Contracts.AssemblyInfo.cs
+++ ClickPremium.Contracts/obj/Debug/net7.0/ClickPremium.Contracts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClickPremium.Contracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a115cb9632072476566498352be207ca56ae5b29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcfd8ce92f2ea4ad6bdc606bd26b0c86ca252778")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClickPremium.Contracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClickPremium.Contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
